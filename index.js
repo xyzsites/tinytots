b4731141097a1b179bec364571b64e12a15cfe28
xyzsites <nova.virell@gmail.com>
2025-11-18 20:48:48 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+    const toggle = document.getElementById('darkModeToggle');
+    const body = document.body;
+    if (localStorage.getItem('theme') === 'light') {
+      body.classList.add('light-mode');
+      toggle.checked = false;
+    }
+    toggle.addEventListener('change', () => {
+      if (toggle.checked) {
+        body.classList.remove('light-mode');
+        localStorage.setItem('theme', 'dark');
+      } else {
+        body.classList.add('light-mode');
+        localStorage.setItem('theme', 'light');
+      }
+    });
